9d80c3546cde5eb88a90d37d536ca76e24dfa946
TheMrYess <79982962+TheMrYess@users.noreply.github.com>
2021-03-11 11:44:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array===null||array===undefined||array.length ==0) return 0;
+  else  return Math.min.apply(null,array);
+            
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array===null||array===undefined||array.length ==0) return 0; else
+  return Math.max.apply(null,array);
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array===null||array===undefined||array.length ==0) return 0; else{
+
+  
+  var i,
+  sum=0;
+  
+for (i=0;i<array.length;i++){
+sum+=array[i];}
+return sum/array.length;}
 }
